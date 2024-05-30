fb9806cb49b0aea1e37b529f38dd7bd02d5cd6f9
Ali khan <khanali6861@gmail.com>
2024-05-30 19:32:35 +0530

    deployement changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import Addcontacts from './components/Addcontacts';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
+import uuid4 from 'uuid4';
+
 
 function App() {
   const [contact , setcontact]=useState([])
  const addcontact=(data)=>{
-  setcontact([...contact,data])
+  setcontact([...contact,{id:uuid4(),data}])
+ }
+ const removecontact=(id)=>{
+  const updatelist=contact.filter((val)=>{
+    return val.id!==id;
+  })
+  setcontact(updatelist);
  }
   return (
     <div >
    <Header/>
    <Addcontacts addcontact={addcontact}/>
-   <ContactList contact={contact}/>
+   <ContactList contact={contact} removecontact={removecontact}/>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const ContactList = ({contact}) => {
-    const contactList=contact.map((val)=>{
-        return(<div className='contact'>
-              <div style={{marginLeft:"2px"}}>{val.name}</div>
-            <div>{val.email}</div>
-        </div>
-          
-        )
-    })
+const ContactList = (props) => {
+  const { contact, removecontact } = props;
+
+  const contactList = contact.map((val) => {
+    return (
+      <div className='contact' key={val.data.email}>
+        <div style={{ marginLeft: '2px' }}>{val.data.name}</div>
+        <div>{val.data.email}</div>
+        <span onClick={() => removecontact(val.id)}>
+          <DeleteIcon />
+        </span>
+      </div>
+    );
+  });
+
   return (
     <>
-<div className='contacthead'>Contact List</div>
-<div>{contactList}</div>
+      <div className='contacthead'>Contact List</div>
+      <div>{contactList}</div>
     </>
-  
-  )
-}
+  );
+};
 
-export default ContactList
+export default ContactList;
